aecb7584067112a352e03bce2de274754fdae3f6
Darya Sharnevich <darya.sharnevich@gmail.com>
2021-12-14 12:48:26 +0300

    main js file created

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+//ПОЛНОЭКРАННОЕ МЕНЮ 
+
+const openFullscreenMenu = document.querySelector('#hamburger')
+const closeFullscreenMenu = document.querySelector('#FSmenuClose');
+const FullscreenMenu = document.querySelector('#FSmenu');
+
+openFullscreenMenu.addEventListener('click', function(element){
+  FullscreenMenu.style.display = 'block';
+
+  closeFullscreenMenu.addEventListener('click', function(element){
+  element.preventDefault();
+  FullscreenMenu.style.display = 'none';
+  });
+});
+
+// ФОРМА
+
 //Проверка имени (нет ли цифр)
 const isName = document.querySelector('#name')
 isName.addEventListener('keydown', function(event) {
@@ -122,6 +139,61 @@ form.addEventListener('submit', e => {
     console.log('нет');
   }
 });
+
+// СЛАЙДЕР 
+
+const slider = document.querySelector('.slider__list');
+const sliderBtn = document.querySelectorAll('.arrow');
+const sliderItems = document.querySelectorAll('.slider__item');
+
+const sliderSwipe = (direction) => {
+  const sliderWidth = slider.offsetWidth;
+
+  let i = 0;
+
+  for (let index = 0; index < sliderItems.length; index++) {
+    const element = sliderItems[index];
+    if (element.classList.contains('slider__item--active')) {
+      i = index;
+    }
+    element.classList.remove('slider__item--active');
+  }
+
+  const itemsCounter = sliderItems.length;
+
+  if (direction === 'left') {
+    i-=1;
+
+    if (i<0) {
+      i = itemsCounter - 1;
+    }
+
+  } else {
+   i+=1;
+
+   if (i > itemsCounter - 1) {
+    i = 0;
+    }
+  }
+
+  slider.style.left = `-${sliderWidth * i}px`;
+  sliderItems[i].classList.add('slider__item--active'); 
+}
+
+
+for (let i = 0; i < sliderBtn.length; i++) {
+  const element = sliderBtn[i];
+
+  element.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (this.classList.contains('arrow--left')) {
+      sliderSwipe('left')
+    } else {
+        sliderSwipe('right')
+      }
+  });
+}
+
 
 
 
